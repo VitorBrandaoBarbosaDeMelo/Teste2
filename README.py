854b860dc95c192bbd710a8dab1629dab0fdfd66
Vitor Brandão Barbosa de Melo <vitorbrandaobarbosa@gmail.com>
2025-11-16 14:31:32 +0000

    Ponto 1 e 2/40

---
+++ README.py
@@ -0,0 +1,39 @@
+# 40 Palavras Restritas do Python – Lista
+# <<<<<<< HEAD
+#<<<<<<< HEAD
+#=======
+# Novo teste salvamento pelo CMD
+# """ Preparar (Stage): Diz ao Git quais arquivos alterados você quer incluir no salvamento.
+
+#Bash
+# git add .
+# (O ponto . adiciona todos os arquivos alterados.)
+
+# Salvar (Commit): Registra um instantâneo das alterações no histórico local.
+# Bash
+# git commit -m "Descreva as alterações feitas"
+
+# Enviar (Push): Envia o salvamento (commit) do seu computador para o GitHub.
+# Bash
+# git push
+
+# 1 – False
+# Representa um valor booleano de falso. Essa palavra é usada para indicar que uma condição não é verdadeira.
+# Você pode atribuir o valor de False a uma variável quando quiser indicar que uma condição não foi atingida ainda.
+# Por exemplo, se um vendedor não bateu a meta de vendas, a variável bateu_meta pode ser definida como False.
+
+# bateu_meta = False
+vendedor = ("Joao")
+vendas = 1000
+meta = 500
+if vendas > meta:
+    print("Bateu a meta")
+bateu_meta = False
+
+
+
+
+
+
+
+
